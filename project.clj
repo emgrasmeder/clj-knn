e52fa390d9ae635679eb183a429909cc1c7b0a03
Emily A Grasmeder <emma@gras.org>
2018-08-29 23:51:40 +0200

    Debugging travis failure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/emilyagras/clj-knn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :main ^:skip-aot clj-knn.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
